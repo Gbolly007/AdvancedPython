0b0ee16c6bcbeb1ca58fde887f472c27a2c7b1bb
Ablie Kassama <32126102+akassama@users.noreply.github.com>
2020-06-22 04:21:11 +0300

    Assignment 5

---
+++ Assignment5/As5_Number1/As5_Number1.py
@@ -0,0 +1,8 @@
+# Python Version 3.8
+import AssessmentPDFLib
+
+
+if __name__ == "__main__":
+    # nothing useful
+    # just to make sure your merge doesn’t crash! print("hello")
+    print("hello")
